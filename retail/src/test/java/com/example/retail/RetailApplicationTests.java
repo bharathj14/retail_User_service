67a2f951a40b1722299fa64bb597a911d17a19ae
bharathj14 <bharath.141293@gmail.com>
2024-10-07 17:56:38 +0530

    fixed  circular dependency issues

--- retail/src/test/java/com/example/retail/RetailApplicationTests.java
+++ retail/src/test/java/com/example/retail/RetailApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.retail;
 
+
+//import com.example.retail.filters.JwtAuthenticationFilterTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootTest
 class RetailApplicationTests {
