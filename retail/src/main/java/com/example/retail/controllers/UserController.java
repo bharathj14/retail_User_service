a580046ed056c6921932961e17477adcdcb7fbd0
tejukrish <tejaswini.krish@gmail.com>
2026-08-29 22:51:38 +0530

    new api -teju

--- retail/src/main/java/com/example/retail/controllers/UserController.java
+++ retail/src/main/java/com/example/retail/controllers/UserController.java
@@ -29,6 +29,15 @@ public class UserController {
         return ResponseEntity.ok(currentUser);
     }
 
+    @GetMapping("/teju")
+    public ResponseEntity<User> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        User currentUser = (User) authentication.getPrincipal();
+
+        return ResponseEntity.ok(currentUser);
+    }
+
     @GetMapping("/")
     public ResponseEntity<List<User>> allUsers() {
         List <User> users = userService.allUsers();
